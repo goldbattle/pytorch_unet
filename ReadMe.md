9250550331cf3657326ab74021f81642f5d7b0b7
Patrick Geneva <patfgeneva@gmail.com>
2019-03-10 19:52:19 -0400

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,43 @@
+# Pytorch U-Net on Cityscapes Dataset
+
+
+This repository contains my first try to get a [U-Net](https://arxiv.org/abs/1505.04597) network training from the [Cityscapes dataset](https://www.cityscapes-dataset.com/).
+This ended up being a bit more challenging then I expected as the data processing tools in python are not as straight forward as I expected.
+Within the Pytorch framework, they provide a "dataloader" for the Cityscapes dataset, but it is not really suitable for any segmentation task.
+I built off of there initial code to allow for the same random transforms to be applied to both the rgb image and labels.
+Additionally, the number of classes used for training have been trimmed down and can be easily changed by updating the *mapping* data type within the dataset.py loader.
+
+
+The network outputs a [N, classes, W, H] size tensor which needs to then be converted into a prediction.
+To find the classification for a given pixel, the argmax of the classes responses is calculated for each and correspond to the class.
+Before saving to disk, I convert this classid back into a rgb color to allow for visual comparison to the groundtruth labels.
+
+
+
+
+
+
+
+
+
+## Training the Model
+
+Please look into the `train.py` file for details on the possible arguments.
+You will first need to download the cityscapes dataset and extract it.
+One would normally use the loss type of "segment" if you want to do pixel-wise segmentation.
+The "reconstruction" will try to just reconstruct the rgb label as the output (which is not super usefull in most cases).
+
+~~~
+python3 script_train.py --datadir <path_to_data> --batch_size 4 --num_gpu 1 --losstype segment
+~~~
+
+
+
+
+## Credits
+
+
+
+
+
+
---
+++ datasets.py
@@ -0,0 +1,197 @@
+import os
+import random
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import torchvision.transforms.functional as TF
+from collections import namedtuple
+
+
+class CityscapesDataset(Dataset):
+
+    def __init__(self, root, split='train', mode='fine'):
+
+        self.root = os.path.expanduser(root)
+        self.mode = 'gtFine' if mode == 'fine' else 'gtCoarse'
+        self.images_dir = os.path.join(self.root, 'leftImg8bit', split)
+        self.targets_dir = os.path.join(self.root, self.mode, split)
+        self.split = split
+        self.images = []
+        self.targets = []
+        self.mapping = {
+            0: 0,  # unlabeled
+            1: 0,  # ego vehicle
+            2: 0,  # rect border
+            3: 0,  # out of roi
+            4: 0,  # static
+            5: 0,  # dynamic
+            6: 0,  # ground
+            7: 1,  # road
+            8: 0,  # sidewalk
+            9: 0,  # parking
+            10: 0,  # rail track
+            11: 0,  # building
+            12: 0,  # wall
+            13: 0,  # fence
+            14: 0,  # guard rail
+            15: 0,  # bridge
+            16: 0,  # tunnel
+            17: 0,  # pole
+            18: 0,  # polegroup
+            19: 0,  # traffic light
+            20: 0,  # traffic sign
+            21: 0,  # vegetation
+            22: 0,  # terrain
+            23: 2,  # sky
+            24: 0,  # person
+            25: 0,  # rider
+            26: 3,  # car
+            27: 0,  # truck
+            28: 0,  # bus
+            29: 0,  # caravan
+            30: 0,  # trailer
+            31: 0,  # train
+            32: 0,  # motorcycle
+            33: 0,  # bicycle
+            -1: 0  # licenseplate
+        }
+        self.mappingrgb = {
+            0: (255, 0, 0),  # unlabeled
+            1: (255, 0, 0),  # ego vehicle
+            2: (255, 0, 0),  # rect border
+            3: (255, 0, 0),  # out of roi
+            4: (255, 0, 0),  # static
+            5: (255, 0, 0),  # dynamic
+            6: (255, 0, 0),  # ground
+            7: (0, 255, 0),  # road
+            8: (255, 0, 0),  # sidewalk
+            9: (255, 0, 0),  # parking
+            10: (255, 0, 0),  # rail track
+            11: (255, 0, 0),  # building
+            12: (255, 0, 0),  # wall
+            13: (255, 0, 0),  # fence
+            14: (255, 0, 0),  # guard rail
+            15: (255, 0, 0),  # bridge
+            16: (255, 0, 0),  # tunnel
+            17: (255, 0, 0),  # pole
+            18: (255, 0, 0),  # polegroup
+            19: (255, 0, 0),  # traffic light
+            20: (255, 0, 0),  # traffic sign
+            21: (255, 0, 0),  # vegetation
+            22: (255, 0, 0),  # terrain
+            23: (0, 0, 255),  # sky
+            24: (255, 0, 0),  # person
+            25: (255, 0, 0),  # rider
+            26: (255, 255, 0),  # car
+            27: (255, 0, 0),  # truck
+            28: (255, 0, 0),  # bus
+            29: (255, 0, 0),  # caravan
+            30: (255, 0, 0),  # trailer
+            31: (255, 0, 0),  # train
+            32: (255, 0, 0),  # motorcycle
+            33: (255, 0, 0),  # bicycle
+            -1: (255, 0, 0)  # licenseplate
+        }
+
+        # =============================================
+        # Check that inputs are valid
+        # =============================================
+        if mode not in ['fine', 'coarse']:
+            raise ValueError('Invalid mode! Please use mode="fine" or mode="coarse"')
+        if mode == 'fine' and split not in ['train', 'test', 'val']:
+            raise ValueError('Invalid split for mode "fine"! Please use split="train", split="test" or split="val"')
+        elif mode == 'coarse' and split not in ['train', 'train_extra', 'val']:
+            raise ValueError('Invalid split for mode "coarse"! Please use split="train", split="train_extra" or split="val"')
+        if not os.path.isdir(self.images_dir) or not os.path.isdir(self.targets_dir):
+            raise RuntimeError('Dataset not found or incomplete. Please make sure all required folders for the'
+                               ' specified "split" and "mode" are inside the "root" directory')
+
+        # =============================================
+        # Read in the paths to all images
+        # =============================================
+        for city in os.listdir(self.images_dir):
+            img_dir = os.path.join(self.images_dir, city)
+            target_dir = os.path.join(self.targets_dir, city)
+            for file_name in os.listdir(img_dir):
+                self.images.append(os.path.join(img_dir, file_name))
+                target_name = '{}_{}'.format(file_name.split('_leftImg8bit')[0], '{}_labelIds.png'.format(self.mode))
+                # target_name = '{}_{}'.format(file_name.split('_leftImg8bit')[0], '{}_color.png'.format(self.mode))
+                self.targets.append(os.path.join(target_dir, target_name))
+
+    def __repr__(self):
+        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
+        fmt_str += '    Number of images: {}\n'.format(self.__len__())
+        fmt_str += '    Split: {}\n'.format(self.split)
+        fmt_str += '    Mode: {}\n'.format(self.mode)
+        fmt_str += '    Root Location: {}\n'.format(self.root)
+        return fmt_str
+
+    def __len__(self):
+        return len(self.images)
+
+    def mask_to_class(self, mask):
+        maskimg = torch.zeros((mask.size()[0], mask.size()[1]), dtype=torch.uint8)
+        for k in self.mapping:
+            maskimg[mask == k] = self.mapping[k]
+        return maskimg
+
+    def mask_to_rgb(self, mask):
+        rgbimg = torch.zeros((3, mask.size()[0], mask.size()[1]), dtype=torch.uint8)
+        for k in self.mappingrgb:
+            rgbimg[0][mask == k] = self.mappingrgb[k][0]
+            rgbimg[1][mask == k] = self.mappingrgb[k][1]
+            rgbimg[2][mask == k] = self.mappingrgb[k][2]
+        return rgbimg
+
+    def class_to_rgb(self, mask):
+        mask2class = dict((v, k) for k, v in self.mapping.items())
+        rgbimg = torch.zeros((3, mask.size()[0], mask.size()[1]), dtype=torch.uint8)
+        for k in mask2class:
+            rgbimg[0][mask == k] = self.mappingrgb[mask2class[k]][0]
+            rgbimg[1][mask == k] = self.mappingrgb[mask2class[k]][1]
+            rgbimg[2][mask == k] = self.mappingrgb[mask2class[k]][2]
+        return rgbimg
+
+    def __getitem__(self, index):
+
+        # first load the RGB image
+        image = Image.open(self.images[index]).convert('RGB')
+
+        # next load the target
+        target = Image.open(self.targets[index]).convert('L')
+
+        # Resize
+        image = TF.resize(image, size=(128+10, 256+10), interpolation=Image.BILINEAR)
+        target = TF.resize(target, size=(128+10, 256+10), interpolation=Image.NEAREST)
+
+        # Random crop
+        i, j, h, w = transforms.RandomCrop.get_params(image, output_size=(128, 256))
+        image = TF.crop(image, i, j, h, w)
+        target = TF.crop(target, i, j, h, w)
+
+        # Random horizontal flipping
+        if random.random() > 0.5:
+            image = TF.hflip(image)
+            target = TF.hflip(target)
+
+        # Random vertical flipping
+        if random.random() > 0.5:
+            image = TF.vflip(image)
+            target = TF.vflip(target)
+
+        # convert to pytorch tensors
+        # target = TF.to_tensor(target)
+        target = torch.from_numpy(np.array(target, dtype=np.uint8))
+        image = TF.to_tensor(image)
+
+        # convert the labels into a mask
+        targetrgb = self.mask_to_rgb(target)
+        targetmask = self.mask_to_class(target)
+        targetmask = targetmask.long()
+        targetrgb = targetrgb.long()
+
+        # finally return the image pair
+        return image, targetmask, targetrgb
